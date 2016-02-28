08d7c9ba43ff2a62a5302d634d9a9b6a7f5b35fd
Claudio Angeloni <angeloniclaudio@alice.it>
2016-02-28 22:31:05 +0100

    test

--- main.py
+++ main.py
@@ -58,11 +58,7 @@ class DomoticXApp(App):
         Clock.schedule_interval(self._update_clock, 1 / 60.)
         self.screens = {}
         self.available_screens = sorted([
-            'Light', 'Buttons', 'ToggleButton', 'Sliders', 'ProgressBar', 'Switches',
-            'CheckBoxes', 'TextInputs', 'Accordions', 'FileChoosers',
-            'Carousel', 'Bubbles', 'CodeInput', 'DropDown', 'Spinner',
-            'Scatter', 'Splitter', 'TabbedPanel + Layouts', 'RstDocument',
-            'Popups', 'ScreenManager'])
+            'Lights', 'Temperatures', 'Scenarios'])
         self.screen_names = self.available_screens
         curdir = dirname(__file__)
         self.available_screens = [join(curdir, 'data', 'screens',
@@ -96,15 +92,6 @@ class DomoticXApp(App):
         self.index = idx
         self.root.ids.sm.switch_to(self.load_screen(idx), direction='left')
 
-    def go_hierarchy_previous(self):
-        ahr = self.hierarchy
-        if len(ahr) == 1:
-            return
-        if ahr:
-            ahr.pop()
-        if ahr:
-            idx = ahr.pop()
-            self.go_screen(idx)
 
     def load_screen(self, index):
         if index in self.screens:
@@ -113,98 +100,6 @@ class DomoticXApp(App):
         self.screens[index] = screen
         return screen
 
-
-    def showcase_floatlayout(self, layout):
-
-        def add_button(*t):
-            if not layout.get_parent_window():
-                return
-            if len(layout.children) > 5:
-                layout.clear_widgets()
-            layout.add_widget(Builder.load_string('''
-#:import random random.random
-Button:
-    size_hint: random(), random()
-    pos_hint: {'x': random(), 'y': random()}
-    text:
-        'size_hint x: {} y: {}\\n pos_hint x: {} y: {}'.format(\
-            self.size_hint_x, self.size_hint_y, self.pos_hint['x'],\
-            self.pos_hint['y'])
-'''))
-            Clock.schedule_once(add_button, 1)
-        Clock.schedule_once(add_button)
-
-    def showcase_boxlayout(self, layout):
-
-        def add_button(*t):
-            if not layout.get_parent_window():
-                return
-            if len(layout.children) > 5:
-                layout.orientation = 'vertical'\
-                    if layout.orientation == 'horizontal' else 'horizontal'
-                layout.clear_widgets()
-            layout.add_widget(Builder.load_string('''
-Button:
-    text: self.parent.orientation if self.parent else ''
-'''))
-            Clock.schedule_once(add_button, 1)
-        Clock.schedule_once(add_button)
-
-    def showcase_gridlayout(self, layout):
-
-        def add_button(*t):
-            if not layout.get_parent_window():
-                return
-            if len(layout.children) > 15:
-                layout.rows = 3 if layout.rows is None else None
-                layout.cols = None if layout.rows == 3 else 3
-                layout.clear_widgets()
-            layout.add_widget(Builder.load_string('''
-Button:
-    text:
-        'rows: {}\\ncols: {}'.format(self.parent.rows, self.parent.cols)\
-        if self.parent else ''
-'''))
-            Clock.schedule_once(add_button, 1)
-        Clock.schedule_once(add_button)
-
-    def showcase_stacklayout(self, layout):
-        orientations = ('lr-tb', 'tb-lr',
-                        'rl-tb', 'tb-rl',
-                        'lr-bt', 'bt-lr',
-                        'rl-bt', 'bt-rl')
-
-        def add_button(*t):
-            if not layout.get_parent_window():
-                return
-            if len(layout.children) > 11:
-                layout.clear_widgets()
-                cur_orientation = orientations.index(layout.orientation)
-                layout.orientation = orientations[cur_orientation - 1]
-            layout.add_widget(Builder.load_string('''
-Button:
-    text: self.parent.orientation if self.parent else ''
-    size_hint: .2, .2
-'''))
-            Clock.schedule_once(add_button, 1)
-        Clock.schedule_once(add_button)
-
-    def showcase_anchorlayout(self, layout):
-
-        def change_anchor(self, *l):
-            if not layout.get_parent_window():
-                return
-            anchor_x = ('left', 'center', 'right')
-            anchor_y = ('top', 'center', 'bottom')
-            if layout.anchor_x == 'left':
-                layout.anchor_y = anchor_y[anchor_y.index(layout.anchor_y) - 1]
-            layout.anchor_x = anchor_x[anchor_x.index(layout.anchor_x) - 1]
-
-            Clock.schedule_once(change_anchor, 1)
-        Clock.schedule_once(change_anchor, 1)
-
-    def _update_clock(self, dt):
-        self.time = time()
 
 if __name__ == '__main__':
     DomoticXApp().run()
